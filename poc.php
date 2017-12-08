5b0f94a934096470a12a997aaf00dfc8d0deb13b
Yannick Lapp <yannick.lapp@cn-consult.eu>
2017-12-08 11:31:28 +0100

    Extend proof of concept
    Made the proof of concept create one entry per table to test the whole database.

--- poc.php
+++ poc.php
@@ -12,11 +12,14 @@ $loader->addPsr4("PizzaService\\", __DIR__);
 // Initialize Propel with the runtime configuration
 Propel::init(__DIR__ . "/propel/conf/pizza_service-conf.php");
 
+use PizzaService\Propel\Models\Customer;
+use PizzaService\Propel\Models\Order;
+use PizzaService\Propel\Models\PizzaOrder;
 use PizzaService\Propel\Models\Pizza;
 use PizzaService\Propel\Models\Ingredient;
 use PizzaService\Propel\Models\PizzaIngredient;
 
-// Adds a test pizza with one ingredient to the database
+// Adds one entry for each table
 $pizza = new Pizza();
 
 $ingredient = new Ingredient();
@@ -29,5 +32,22 @@ $ingredientDough->setGrams(1000)
 $pizza->setName("Pizzateig pur")
       ->setOrderCode(1)
       ->setPrice(4.50)
-      ->addPizzaIngredient($ingredientDough)
+      ->addPizzaIngredient($ingredientDough);
+
+$customer = new Customer();
+$customer->setFirstName("Test")
+         ->setLastName("Käufer")
+         ->setCountry("Deutschland")
+         ->setZip("11111")
+         ->setCity("Einshausen")
+         ->setStreetName("Einsstraße")
+         ->setHouseNumber(1);
+
+$pizzaOrder = new PizzaOrder();
+$pizzaOrder->setPizza($pizza)
+           ->setAmount(1);
+
+$order = new Order();
+$order->setCustomer($customer)
+      ->addPizzaOrder($pizzaOrder)
       ->save();
